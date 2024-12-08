8f990eb10a9824fe9cbace2b765d39f816d9501d
Sowparna Shome <soupornoshome@gmail.com>
2024-12-08 16:12:14 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Buy_a_Sticker_v1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
